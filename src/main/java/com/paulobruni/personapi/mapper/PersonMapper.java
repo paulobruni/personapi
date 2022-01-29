41bcf6296b82d5ef715631c1083156978ff9e817
paulobruni <paulovitor182@hotmail.com>
2022-01-29 02:02:20 -0300

    Utilização de DTO, validação de CPF, implementação com MapStruct (Mapper)

---
+++ src/main/java/com/paulobruni/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.paulobruni.personapi.mapper;
+
+import com.paulobruni.personapi.Entity.Person;
+import com.paulobruni.personapi.dto.request.PersonDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+
+}
--- src/main/java/com/paulobruni/personapi/service/PersonService.java
+++ src/main/java/com/paulobruni/personapi/service/PersonService.java
@@ -1,7 +1,9 @@
 package com.paulobruni.personapi.service;
 
 import com.paulobruni.personapi.Entity.Person;
+import com.paulobruni.personapi.dto.request.PersonDTO;
 import com.paulobruni.personapi.dto.response.MessageResponseDTO;
+import com.paulobruni.personapi.mapper.PersonMapper;
 import com.paulobruni.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,14 +15,18 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person)
+    public MessageResponseDTO createPerson(PersonDTO personDTO)
     {
-        Person savedPerson = personRepository.save(person);
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
