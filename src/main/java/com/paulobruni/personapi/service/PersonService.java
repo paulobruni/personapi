a741e1f9a2602ff121ac7a2c4a946b854ff5cbae
paulobruni <paulovitor182@hotmail.com>
2022-01-29 02:09:09 -0300

    Listando Pessoas

--- src/main/java/com/paulobruni/personapi/service/PersonService.java
+++ src/main/java/com/paulobruni/personapi/service/PersonService.java
@@ -7,8 +7,12 @@ import com.paulobruni.personapi.mapper.PersonMapper;
 import com.paulobruni.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -33,4 +37,10 @@ public class PersonService {
                 .build();
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople =  personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 }
